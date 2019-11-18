2c2a4873210f462b8280aac9b4a008ccee06a77d
fattahyousaini <18016553@rp.edu.sg>
2019-11-18 15:47:08 +0800

    Quiz

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import Getaboatapp from './Getaboatapp';
 import {name as appName} from './app.json';
+import Lesson05quiz from './Lesson05quiz';
 
-AppRegistry.registerComponent(appName, () => Getaboatapp);
+AppRegistry.registerComponent(appName, () => Lesson05quiz);
